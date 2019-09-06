f10a06802cb02cedba8805716bba043c9ad24dcc
Vladimir Samojlov <v-moi@ya.ru>
2019-09-06 15:17:48 +0300

    edit form

--- blocks/schedule/schedule-form.php
+++ blocks/schedule/schedule-form.php
@@ -12,14 +12,33 @@
 	</div>
 </div>
 <div class="fizmat__form">
-	<div class="fizmat__item">
-		<span>Город</span>
-		<div class="select-wrapper">
-			<select class="select select-schedule-city">
-				<option value="Харьков" selected>Харьков</option>
-				<option value="Киев">Киев</option>
-			</select>
+	<?php
+		$body_classes = get_body_class();
+		if (in_array('post-type-archive-lessons',$body_classes)) {
+			$show_city_filter = true;
+		}
+	?>
+	<?php if($show_city_filter): ?>
+		<div class="fizmat__item">
+			<span>Город</span>
+			<div class="select-wrapper">
+				<select class="select select-schedule-city">
+					<option value="Харьков" selected>Харьков</option>
+					<option value="Киев">Киев</option>
+				</select>
+			</div>
 		</div>
+	<?php endif; ?>
+	<div class="fizmat__item">
+		<span>Класс</span>
+		<div class="select-wrapper">
+	    <select class="select select-schedule-class">
+	    	<?php $class_cats = get_terms( array( 'taxonomy' => 'class', 'parent' => 0, 'hide_empty' => false ) );
+	    	foreach ( $class_cats as $class_cat ): ?>
+	    		<option value="<?php echo $class_cat->term_id ?>"><?php echo $class_cat->name ?></option>
+	    	<?php endforeach; ?>
+	    </select>
+	  </div>
 	</div>
 	<div class="fizmat__item">
 		<span>Преподаватель</span>
@@ -33,17 +52,6 @@
 					if ($custom_query_teachers->have_posts()) : while ($custom_query_teachers->have_posts()) : $custom_query_teachers->the_post(); ?>
 	    		<option value="<?php echo get_the_id(); ?>"><?php the_title(); ?></option>
 	    	<?php endwhile; endif; wp_reset_postdata(); ?>
-	    </select>
-	  </div>
-	</div>
-	<div class="fizmat__item">
-		<span>Класс</span>
-		<div class="select-wrapper">
-	    <select class="select select-schedule-class">
-	    	<?php $class_cats = get_terms( array( 'taxonomy' => 'class', 'parent' => 0, 'hide_empty' => false ) );
-	    	foreach ( $class_cats as $class_cat ): ?>
-	    		<option value="<?php echo $class_cat->term_id ?>"><?php echo $class_cat->name ?></option>
-	    	<?php endforeach; ?>
 	    </select>
 	  </div>
 	</div>
